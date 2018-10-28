314e53e13cc1082df32197d0931660d2eac24db1
赵明文 <zhaomingwen@zhaominwendeMBP.lan>
2018-10-28 13:51:10 +0800

    forth part without general templates

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,9 @@
 # Create your views here.
 
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, Http404
-from .models import Question
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from .models import Question, Choice
+from django.urls import reverse
 #from django.template import loader
 
 
@@ -41,8 +42,21 @@ def detail(request, question_id):
     return render(request, 'polls/detail.html', {'question': question})
     
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # 重新显示问题的投票表单
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # 成功处理之后 POST 数据之后，总是返回一个 HttpResponseRedirect 。防止因为用户点击了后退按钮而提交了两次。
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
